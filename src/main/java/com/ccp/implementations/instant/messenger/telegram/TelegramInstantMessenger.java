81058e85bb7d460979cf98a52b3b220a408008cb
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-08-14 21:55:07 -0300

    declaração de guerra às strings literais

--- src/main/java/com/ccp/implementations/instant/messenger/telegram/TelegramInstantMessenger.java
+++ src/main/java/com/ccp/implementations/instant/messenger/telegram/TelegramInstantMessenger.java
@@ -18,22 +18,24 @@ import com.ccp.exceptions.instant.messenger.CcpErrorInstantMessageThisBotWasBloc
 import com.ccp.exceptions.instant.messenger.CcpErrorInstantMessageTooManyRequests;
 import com.ccp.http.CcpHttpMethods;
 import com.ccp.process.CcpFunctionThrowException;
-
+enum TelegramInstantMessengerConstants{
+	chatId, ok, result, recipient, message, method, replyTo, reply_to_message_id, parse_mode, chat_id, text, url, message_id, token
+}
 class TelegramInstantMessenger implements CcpInstantMessenger {
 	
 	public Long getMembersCount(CcpJsonRepresentation parameters) {
 		CcpHttpRequester ccpHttp = CcpDependencyInjection.getDependency(CcpHttpRequester.class);
 
-		Long chatId = parameters.getAsLongNumber("chatId");
+		Long chatId = parameters.getAsLongNumber(TelegramInstantMessengerConstants.chatId);
 		String url = this.getCompleteUrl(parameters);
 		ccpHttp.executeHttpRequest(url + "/getChatMemberCount?chat_id=" + chatId, CcpHttpMethods.GET, CcpOtherConstants.EMPTY_JSON, "", 200);
 		CcpHttpHandler ccpHttpHandler = new CcpHttpHandler(200);
 
 		CcpJsonRepresentation response = ccpHttpHandler.executeHttpSimplifiedGet("getMembersCount", url, CcpHttpResponseType.singleRecord);
-		if(response.getAsBoolean("ok") == false) {
+		if(response.getAsBoolean(TelegramInstantMessengerConstants.ok) == false) {
 			throw new CcpErrorInstantMessengerChatErrorCount(chatId);
 		}
-		Long result = response.getAsLongNumber("result");
+		Long result = response.getAsLongNumber(TelegramInstantMessengerConstants.result);
 		return result;
 	}
 	CcpInstantMessenger throwThisBotWasBlockedByThisUser(String token) {
@@ -48,7 +50,7 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 		
 //		this.throwTooManyRequests();
 //		this.throwThisBotWasBlockedByThisUser(token);
-		Long chatId = json.getAsLongNumber("recipient");
+		Long chatId = json.getAsLongNumber(TelegramInstantMessengerConstants.recipient);
 		Set<String> fieldSet = json.fieldSet();
 		for (String fieldName : fieldSet) {
 			Object value = json.get(fieldName);
@@ -56,7 +58,7 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 				json = json.put(fieldName, value.toString());
 			}
 		}
-		String message = json.getAsString("message")
+		String message = json.getAsString(TelegramInstantMessengerConstants.message)
 				.replace("\u003cBR\u003e", "\n")
 				.replace("\u003cbr\u003e", "\n")
 				.replace("<BR/>", "\n")
@@ -70,7 +72,7 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 				.replace("<", "(")
 				.replace(">", ")")
 				;
-		Long replyTo =  json.getOrDefault("replyTo", 0L);
+		Long replyTo =  json.getOrDefault(TelegramInstantMessengerConstants.replyTo, 0L);
 
 		if(message.trim().isEmpty()) {
 			return CcpOtherConstants.EMPTY_JSON;
@@ -88,28 +90,28 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 		}
 		String botUrl = this.getCompleteUrl(json);
 		String url = botUrl + "/sendMessage";
-		CcpHttpMethods method = CcpHttpMethods.valueOf( json.getAsString("method"));
+		CcpHttpMethods method = CcpHttpMethods.valueOf( json.getAsString(TelegramInstantMessengerConstants.method));
 		
 		CcpJsonRepresentation handlers = CcpOtherConstants.EMPTY_JSON
-				.addJsonTransformer("403", new CcpFunctionThrowException(new CcpErrorInstantMessageThisBotWasBlockedByThisUser(token)))
-				.addJsonTransformer("429", new CcpFunctionThrowException(new CcpErrorInstantMessageTooManyRequests()))
-				.addJsonTransformer("200", CcpOtherConstants.DO_NOTHING)
+				.addJsonTransformer(403, new CcpFunctionThrowException(new CcpErrorInstantMessageThisBotWasBlockedByThisUser(token)))
+				.addJsonTransformer(429, new CcpFunctionThrowException(new CcpErrorInstantMessageTooManyRequests()))
+				.addJsonTransformer(200, CcpOtherConstants.DO_NOTHING)
 				;
 		
 		CcpHttpHandler ccpHttpHandler = new CcpHttpHandler(handlers);
 		
 		for (String text : texts) {
 			CcpJsonRepresentation body = CcpOtherConstants.EMPTY_JSON
-					.put("reply_to_message_id", replyTo)
-					.put("parse_mode", "html")
-					.put("chat_id", chatId)
-					.put("text", text);
+					.put(TelegramInstantMessengerConstants.reply_to_message_id, replyTo)
+					.put(TelegramInstantMessengerConstants.parse_mode, "html")
+					.put(TelegramInstantMessengerConstants.chat_id, chatId)
+					.put(TelegramInstantMessengerConstants.text, text);
 			
 			CcpJsonRepresentation response = ccpHttpHandler.executeHttpRequest("sendInstantMessage", url, method, CcpOtherConstants.EMPTY_JSON, body, CcpHttpResponseType.singleRecord);
 			
-			CcpJsonRepresentation result = response.getInnerJson("result");
+			CcpJsonRepresentation result = response.getInnerJson(TelegramInstantMessengerConstants.result);
 			
-			replyTo = result.getAsLongNumber("message_id");
+			replyTo = result.getAsLongNumber(TelegramInstantMessengerConstants.message_id);
 		}
 		
 		return CcpOtherConstants.EMPTY_JSON
@@ -122,7 +124,7 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 		CcpJsonRepresentation properties = new CcpStringDecorator("application_properties").propertiesFrom().environmentVariablesOrClassLoaderOrFile();	
 		String tokenValue = this.getToken(parameters);
 		
-		String urlKey = parameters.getAsString("url");
+		String urlKey = parameters.getAsString(TelegramInstantMessengerConstants.url);
 		String urlValue = properties.getAsString(urlKey);
 		
 		return urlValue + tokenValue;
@@ -130,7 +132,7 @@ class TelegramInstantMessenger implements CcpInstantMessenger {
 
 	public String getToken(CcpJsonRepresentation parameters) {
 		CcpJsonRepresentation properties = new CcpStringDecorator("application_properties").propertiesFrom().environmentVariablesOrClassLoaderOrFile();	
-		String tokenKey = parameters.getAsString("token");
+		String tokenKey = parameters.getAsString(TelegramInstantMessengerConstants.token);
 		String tokenValue = properties.getAsString(tokenKey);
 		if(tokenValue.trim().isEmpty()) {
 			return tokenKey;
